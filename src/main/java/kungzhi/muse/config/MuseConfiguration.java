2d12e1a1bb0a97a84ef9f35315c961e67f4d2e9c
Jeffrey Glatz <jglatz@MacBook.fios-router.home>
2017-11-06 18:56:15 -0600

    Muse: Integrate SpringBoot

--- src/main/java/kungzhi/muse/config/MuseConfiguration.java
+++ src/main/java/kungzhi/muse/config/MuseConfiguration.java
@@ -1,13 +1,6 @@
 package kungzhi.muse.config;
 
-import kungzhi.muse.osc.BandPowerTransformer;
-import kungzhi.muse.osc.BatteryTransformer;
-import kungzhi.muse.osc.ConfigurationTransformer;
-import kungzhi.muse.osc.DrlReferenceTransformer;
-import kungzhi.muse.osc.EegTransformer;
-import kungzhi.muse.osc.MessageDispatcher;
-import kungzhi.muse.osc.SessionScoreTransformer;
-import kungzhi.muse.osc.VersionTransformer;
+import kungzhi.muse.osc.*;
 import kungzhi.muse.repository.Bands;
 import kungzhi.muse.stream.ConfigurationStream;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -70,28 +63,22 @@ public class MuseConfiguration {
                 .streaming("/muse/elements/gamma_absolute",
                         new BandPowerTransformer(bands.load("gamma"), false),
                         (session, model) -> {
-                            System.out.println("G:" + model.average());
                         })
                 .streaming("/muse/elements/delta_relative",
                         new BandPowerTransformer(bands.load("delta"), true),
                         (session, model) -> {
-                            System.out.println("D:" + model.average());
                         })
                 .streaming("/muse/elements/theta_relative",
                         new BandPowerTransformer(bands.load("theta"), true),
                         (session, model) -> {
-                            System.out.println("T:" + model.average());
-
                         })
                 .streaming("/muse/elements/alpha_relative",
                         new BandPowerTransformer(bands.load("alpha"), true),
                         (session, model) -> {
-                            System.out.println("A:" + model.average());
                         })
                 .streaming("/muse/elements/beta_relative",
                         new BandPowerTransformer(bands.load("beta"), true),
                         (session, model) -> {
-                            System.out.println("B:" + model.average());
                         })
                 .streaming("/muse/elements/gamma_relative",
                         new BandPowerTransformer(bands.load("gamma"), true),
